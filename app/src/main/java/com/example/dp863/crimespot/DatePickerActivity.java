f1082a8ddee6adf9ce1e6d2362efe1a7515866b0
dp8634 <dp8634@gmail.com>
2017-04-02 14:13:54 -0400

    Update DatePickerActivity.java

--- app/src/main/java/com/example/dp863/crimespot/DatePickerActivity.java
+++ app/src/main/java/com/example/dp863/crimespot/DatePickerActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,12 +21,15 @@ import java.lang.Override;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.Date;
 
 public class DatePickerActivity extends AppCompatActivity implements View.OnClickListener {
 
     //    UI References
     private EditText fromDateEtxt;
     private EditText toDateEtxt;
+    Date fromDate, toDate;
+
     private ImageButton mImageButton1;
     private ImageButton mImageButton2;
 
@@ -74,6 +78,10 @@ public class DatePickerActivity extends AppCompatActivity implements View.OnClic
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
                 fromDateEtxt.setText(dateFormatter.format(newDate.getTime()));
+                //Date fromDate = newDate.getTime();
+
+
+                //Log.e("My App", "From Date" + fromDateEtxt.getText() );
             }
 
         },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
@@ -84,6 +92,7 @@ public class DatePickerActivity extends AppCompatActivity implements View.OnClic
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
                 toDateEtxt.setText(dateFormatter.format(newDate.getTime()));
+
             }
 
         },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
@@ -103,6 +112,15 @@ public class DatePickerActivity extends AppCompatActivity implements View.OnClic
         if (view == mImageButton1) {
             Toast.makeText(DatePickerActivity.this, "Go to Map", Toast.LENGTH_SHORT).show();
             Intent mIntent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(mIntent);
+        }
+
+        //save dates and return to map
+        if (view == mImageButton2)  {
+            Toast.makeText(DatePickerActivity.this, "Saved Settings", Toast.LENGTH_SHORT).show();
+            Intent mIntent = new Intent(getApplicationContext(), MapsActivity.class);
+            mIntent.putExtra("fromDate", fromDateEtxt.getText().toString());
+            mIntent.putExtra("toDate", toDateEtxt.getText().toString());
             startActivity(mIntent);
         }
     }
